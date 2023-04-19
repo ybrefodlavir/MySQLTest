3d0d9269b9c1ff96022b80b5582916cc5cf3b744
ybrefodlavir <rivaldoferby69@gmail.com>
2023-04-19 13:07:55 +0700

    insert update delete done

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,46 +16,29 @@ class TestController extends Controller
         $question_id = $request->input('question_id');
         $attempt_count = Answer::where([['student_id', 1], ['question_id', $question_id]])->count();
         $attempt_count = $attempt_count != 0 ? $attempt_count + 1 : 1;
-
-
         $code = $request->input('code');
         try {
             if (preg_match('(drop|create|alter|select)i', $code) === 1) {
                 return response()->json(['status' => 'error', 'message' => "Code not allowed"]);
             } else if (preg_match('(insert|update|delete)i', $code) === 1) {
                 DB::connection('mysql-test')->statement($code);
-
-
                 // validation here
                 $validation = $this->statementValidation($question_id);
-
                 if ($validation['status'] == false) {
                     DB::connection('mysql-test')->rollBack();
+                    $this->create_answer(1, $question_id, $code, 0, $attempt_count, "Answer is wrong " . $validation['message']);
                     return response()->json(['status' => 'error', 'message' => "Answer is wrong " . $validation['message']]);
                 } else {
                     DB::connection('mysql-test')->rollBack();
-                    Answer::create([
-                        'student_id' => 1,
-                        'question_id' => $question_id,
-                        'code' => $code,
-                        'status' => 1,
-                        'attempt' => $attempt_count,
-                        'message' => 'Code executed successfully'
-                    ]);
+                    $this->create_answer(1, $question_id, $code, 1, $attempt_count, 'Code executed successfully');
                     return response()->json(['status' => 'success', 'message' => "Code executed successfully"]);
                 }
             } else {
+                $this->create_answer(1, $question_id, $code, 0, $attempt_count, "Your code can't be identified");
                 return response()->json(['status' => 'error', 'message' => "Your code can't be identified"]);
             }
         } catch (\Throwable $error) {
-            Answer::create([
-                'student_id' => 1,
-                'question_id' => $question_id,
-                'code' => $code,
-                'status' => 0,
-                'attempt' => $attempt_count,
-                'message' => $error->getMessage()
-            ]);
+            $this->create_answer(1, $question_id, $code, 0, $attempt_count, $error->getMessage());
             DB::connection('mysql-test')->rollBack();
             return response()->json(['status' => 'error', 'message' =>  $error->getMessage()]);
         }
@@ -69,11 +52,19 @@ class TestController extends Controller
             'status' => false,
             'message' => '',
         ];
-        if ($question->type == "insert single") {
+
+        if ($question->type == "insert single" || $question->type == "insert single specific") {
             $query = DB::connection('mysql-test')->table($validation_statement['tableName'])
                 ->selectRaw($validation_statement['selectRaw'])
                 ->whereRaw($validation_statement['whereRaw'])
                 ->first();
+
+            $nullKey = $validation_statement['nullKey'];
+            if ($question->type == "insert single specific" && $query->$nullKey !== null) {
+                $valid['status'] = false;
+                $valid['message'] = $validation_statement['nullKey'] . " must be null";
+                return $valid;
+            }
             foreach ($validation_value as $property => $value) {
                 if ($value !== $query->$property) {
                     $valid['status'] = false;
@@ -83,7 +74,7 @@ class TestController extends Controller
                     $valid['status'] = true;
                 }
             }
-        } else if ($question->type == "insert multiple") {
+        } else if ($question->type == "insert multiple" || $question->type == "insert multiple specific") {
             $query = DB::connection('mysql-test')->table($validation_statement['tableName'])
                 ->selectRaw($validation_statement['selectRaw'])
                 ->whereRaw($validation_statement['whereRaw'])
@@ -99,7 +90,74 @@ class TestController extends Controller
                     }
                 }
             }
+        } else if ($question->type == "update all") {
+            $query = DB::connection('mysql-test')->table($validation_statement['tableName'])
+                ->selectRaw($validation_statement['selectRaw'])
+                ->get();
+            foreach ($validation_value as $key => $row) {
+                foreach ($row as $property => $value) {
+                    if ($value !== $query[$key]->$property) {
+                        $valid['status'] = false;
+                        $valid['message'] = "The value doesn't match";
+                        return $valid;
+                    } else {
+                        $valid['status'] = true;
+                    }
+                }
+            }
+        } else if ($question->type == "update specific") {
+            $query = DB::connection('mysql-test')->table($validation_statement['tableName'])
+                ->selectRaw($validation_statement['selectRaw'])
+                ->whereRaw($validation_statement['whereRaw'])
+                ->get();
+            foreach ($validation_value as $key => $row) {
+                foreach ($row as $property => $value) {
+                    if ($value !== $query[$key]->$property) {
+                        $valid['status'] = false;
+                        $valid['message'] = "The value doesn't match";
+                        return $valid;
+                    } else {
+                        $valid['status'] = true;
+                    }
+                }
+            }
+        } else if ($question->type == "delete specific") {
+            $query = DB::connection('mysql-test')->table($validation_statement['tableName'])
+                ->selectRaw($validation_statement['selectRaw'])
+                ->whereRaw($validation_statement['whereRaw'])
+                ->get();
+            if (!$query->empty()) {
+                $valid['status'] = false;
+                $valid['message'] = "The data haven't been deleted";
+                return $valid;
+            } else {
+                $valid['status'] = true;
+            }
+        } else if ($question->type == "delete all") {
+            $query = DB::connection('mysql-test')->table($validation_statement['tableName'])
+                ->get();
+            if (!$query->empty()) {
+                $valid['status'] = false;
+                $valid['message'] = "The data haven't been deleted";
+                return $valid;
+            } else {
+                $valid['status'] = true;
+            }
         }
+
+
         return $valid;
+    }
+
+    private function create_answer($student_id, $question_id, $code, $status, $attempt_count, $message)
+    {
+        Answer::create([
+            'student_id' => $student_id,
+            'question_id' => $question_id,
+            'code' => $code,
+            'status' => $status,
+            'attempt' => $attempt_count,
+            'message' => $message
+        ]);
     }
 }
